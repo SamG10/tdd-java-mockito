f5b609965730c832fddd3ca58c15b372f8f27b0d
Samuel <sgeorge@apizr.fr>
2024-06-12 10:54:33 +0200

    tdd green step functionality 1

--- src/main/java/com/example/testingjavamockito/SearchCity.java
+++ src/main/java/com/example/testingjavamockito/SearchCity.java
@@ -2,12 +2,35 @@ package com.example.testingjavamockito;
 
 import jdk.jshell.spi.ExecutionControl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SearchCity {
     private List<String> cities;
 
     public List<String> searchCity(String city) {
+        cities = new ArrayList<>();
+        cities.add("Paris");
+        cities.add("Budapest");
+        cities.add("Skopje");
+        cities.add("Rotterdam");
+        cities.add("Valence");
+        cities.add("Vancouver");
+        cities.add("Amsterdam");
+        cities.add("Vienne");
+        cities.add("Sydney");
+        cities.add("New York");
+        cities.add("Londres");
+        cities.add("Bangkok");
+        cities.add("Hong Kong");
+        cities.add("Dubaï");
+        cities.add("Rome");
+        cities.add("Istanbul");
+
+        if (city.length() < 2) {
+            throw new NotFoundException("City not found, please try again with at least 2 characters minimum");
+        }
+
         throw new UnsupportedOperationException("");
     }
 }
